63507a6f04878b377936adf9a0978852a8d23f84
makrau <iago006@hotmail.com>
2020-04-10 22:08:45 -0300

    corrigido atualizacao de um repositorio

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -11,6 +11,10 @@ const repositories = [];
 
 const checkRepositoryMiddleware = (request, response, next) => {
   const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository ID' });
+  }
 
   const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
 
@@ -25,6 +29,8 @@ const checkRepositoryMiddleware = (request, response, next) => {
 
   return next();
 };
+
+app.use('/repositories/:id', checkRepositoryMiddleware);
 
 app.get('/repositories', (request, response) => {
   response.json(repositories);
@@ -45,31 +51,27 @@ app.post('/repositories', (request, response) => {
   response.json(repository);
 });
 
-app.put('/repositories/:id', checkRepositoryMiddleware, (request, response) => {
+app.put('/repositories/:id', (request, response) => {
   const { title, url, techs } = request.body;
   const { repositoryIndex, repositoryId: id } = request.middlewareData;
 
-  const oldRepository = repositories[repositoryIndex];
-  const updatedRepository = {
-    id,
-    title: title || oldRepository.title,
-    url: url || oldRepository.url,
-    techs: techs || oldRepository.techs,
-  };
+  const repository = repositories[repositoryIndex];
 
-  repositories[repositoryIndex] = updatedRepository;
+  repository.title = title || repository.title;
+  repository.url = url || repository.url;
+  repository.techs = techs || repository.techs;
 
-  return response.json(updatedRepository);
+  return response.json(repository);
 });
 
-app.delete('/repositories/:id', checkRepositoryMiddleware, (request, response) => {
+app.delete('/repositories/:id', (request, response) => {
   const { repositoryIndex } = request.middlewareData;
   repositories.splice(repositoryIndex, 1);
 
-  return response.status(204).json();
+  return response.status(204).send();
 });
 
-app.post('/repositories/:id/like', checkRepositoryMiddleware, (request, response) => {
+app.post('/repositories/:id/like', (request, response) => {
   const { repositoryIndex } = request.middlewareData;
 
   const repository = repositories[repositoryIndex];
